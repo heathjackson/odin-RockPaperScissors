24139ea180ddf58c5810a6b387c1f28336f54c75
Heather Jackson <heather.jackson2003@gmail.com>
2022-08-20 18:14:51 -0400

    getComputerChoice and playerChoice functions committed

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,10 +4,14 @@ function getComputerChoice(arr){
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-console.log(getComputerChoice(["rock","paper","scissor"]))
+// console.log(getComputerChoice(["rock","paper","scissor"]))
 
 // ask the player for their selection
 
 function playerChoice() {
-  player = input
+  const player = prompt("Please choose rock, paper, or scissors");
+  return player.toLowerCase();
 }
+
+// console.log(playerChoice())
+
