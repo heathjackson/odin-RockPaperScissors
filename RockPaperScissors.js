2fb4643c5c9f6f1577d74a3e5f9dc8d1f3ffd350
Heather Jackson <heather.jackson2003@gmail.com>
2022-08-20 15:38:32 -0400

    uploaded files

---
+++ RockPaperScissors.js
@@ -0,0 +1,13 @@
+// randomly chooses a string from the array
+
+function getComputerChoice(arr){
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+console.log(getComputerChoice(["rock","paper","scissor"]))
+
+// ask the player for their selection
+
+function playerChoice() {
+  player = input
+}
