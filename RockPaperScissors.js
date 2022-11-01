6899777c2adb2af0dbc0842ede6e5f20a62e9fe3
Heather Jackson <heather.jackson2003@gmail.com>
2022-11-01 16:53:04 -0400

    everything works well on UI, but need to add async and await once I learn how to use them correctly

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,7 +1,4 @@
 // rock paper scissors game function - player vs computer
-
-
-
 let playerScore = 0;
 let computerScore = 0;
 
@@ -24,100 +21,96 @@ const arr = ["rock", "paper", "scissors"];
 
 // makes the button and the website connect correctly and 
 // connects them to the game function
+
 const rock = document.querySelector('#rock');
-rock.addEventListener('click', () => {
+rock.addEventListener('click', function() {
+  sound();
   game('rock');
-});
+})
 
 const paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
+  sound();
   game('paper');
 });
 
 const scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
-  game('scissors');
-});
-
+  sound();
+  game('scissors')
+})
 
 function game(playerChoice) {
-    let computerSelection = getComputerChoice(arr);
-
-    if (playerScore === 5 || computerScore === 5) {
-      endGame();
-      return;
-    }
-    // console.log("You chose", playerSelection);
-    // console.log("The computer chose", computerSelection);
-    round = playRound(playerChoice, computerSelection);
-   
-    if (round === "win") {
-      playerScore += 1;
-      console.log("You WON this round! You chose " + playerChoice + "," + " and the computer chose " + computerSelection + ".");
-    }
-    else if (round === "lose") {
-      computerScore += 1;
-      console.log("You LOST this round. You chose " + playerChoice + "," + " and the computer chose " + computerSelection + ".");
-    }
-    else {
-      console.log("You TIED this round. You chose " + playerChoice + "," + " and the computer chose " + computerSelection + ".");
-    }
-    playerTotal.textContent = `Your score is ${playerScore}`;
-    computerTotal.textContent = `The computer's score is ${computerScore}`;
-
-    if (playerScore === 5 || computerScore === 5) {
-      endGame();
-      return;
-    }
+  if (playerScore === 5 || computerScore === 5) {
+    endGame();
+    return;
   }
 
-    
+  let computerSelection = getComputerChoice(arr);
+  // console.log("You chose", playerSelection);
+  // console.log("The computer chose", computerSelection);
+  let round = playRound(playerChoice, computerSelection);
   
-  // console.log("player =", playerScore);
-  // console.log("computer =", computerScore);
-function endGame() {
-    if (playerScore > computerScore) {
-      alert("You are the winner, winner, chicken dinner of the game!!!!");
-    }
-    else {
-      alert("YOU LOST THE GAME SUCKA");
-    }
+  if (round === "win") {
+    playerScore += 1;
+    console.log("You WON this round! You chose " + playerChoice + "," + " and the computer chose " + computerSelection + ".");
+  }
+  else if (round === "lose") {
+    computerScore += 1;
+    console.log("You LOST this round. You chose " + playerChoice + "," + " and the computer chose " + computerSelection + ".");
+  }
+  else {
+    console.log("You TIED this round. You chose " + playerChoice + "," + " and the computer chose " + computerSelection + ".");
   }
 
-// FUNCTIONS USED //
+  playerTotal.textContent = `YOUR SCORE = ${playerScore}`;
+  computerTotal.textContent = `COMPUTER'S SCORE = ${computerScore}`;
 
+  if (playerScore === 5 || computerScore === 5) {
+    endGame();
+  }
+}
+
+//*********************** FUNCTIONS USED ******************************//   
+//makes the click sound when the button is pushed
+
+function sound() {
+  var snd = new Audio('sounds/click.m4a')//wav is also supported
+  snd.play()//plays the sound
+  return;
+}
+  
+//alert when the game is over
+
+function endGame() {
+  if ((playerScore === 5 || computerScore === 5) && (playerScore > computerScore)) {
+      alert("You are the winner, winner, chicken dinner of the game!!!!");
+      return;
+  }
+
+  else {
+      alert("YOU LOST THE GAME SUCKA");
+      return;
+    }
+}
 // Plays one round of Rock, Paper, Scissors //
 
 function playRound(playerChoice, computerSelection) {
-    if (playerChoice !== "rock" && playerChoice !== "paper" && playerChoice !== "scissors") {
-      return playerChoice = "wrongChoice";
-    }
-    else if (playerChoice === computerSelection) {
-      return playerChoice = "tie";
-    }
-    else if ((playerChoice === "rock" && computerSelection === "scissors") || (playerChoice === "paper" && computerSelection === "rock") || (playerChoice === "scissors" && computerSelection === "paper")) {
-      return playerChoice = "win";
-    }
-    else {
-      return playerChoice = "lose";
-    }
+  if (playerChoice === computerSelection) {
+    return playerChoice = "tie";
   }
+  else if ((playerChoice === "rock" && computerSelection === "scissors") || (playerChoice === "paper" && computerSelection === "rock") || (playerChoice === "scissors" && computerSelection === "paper")) {
+    return playerChoice = "win";
+  }
+  else {
+    return playerChoice = "lose";
+  }
+}
 
 // randomly chooses a string from the array
 
 function getComputerChoice(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
+  return arr = arr[Math.floor(Math.random() * arr.length)];
 }
-
-// asks the player for their selection
-
-// function playerChoice() {
-//   let player = prompt("Please choose rock, paper, or scissors");
-//   return player.toLowerCase();
-// }
-
-
- //each score is tallied on the website
  
-
 
